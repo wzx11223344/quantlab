10f76ed1d73c4a0a923acf08e9b8cf0f5e038d9c
wzx112233 <3521257027@qq.com>
2026-06-28 17:06:54 +0800

    Add quantlab/__init__.py

---
+++ quantlab/__init__.py
@@ -0,0 +1,8 @@
+"""
+QuantLab: Quantitative Finance Laboratory
+
+A comprehensive financial engineering toolkit with pure NumPy implementations.
+"""
+
+__version__ = "1.0.0"
+__author__ = "QuantLab Contributors"
